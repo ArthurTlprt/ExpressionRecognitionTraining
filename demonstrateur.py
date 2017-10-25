8d86e3191f3dd7bc55e41c0963ee41711dc5ed03
ArthurT <arthur.talpaert@isen-lille.fr>
2017-10-25 13:26:06 +0200

    convertion de l'image de bgr à rgb

--- demonstrateur.py
+++ demonstrateur.py
@@ -13,19 +13,28 @@ cap = cv2.VideoCapture(0)
 # we read the cam indefinitely
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_)
     # find the face
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
     # if a face is found
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         # subarray corresponding to the face
         np_face = img[y:y+h, x:x+w]
         # converting into PIL.Image object to resize
-        pil_face = Image.fromarray(np.uint8(np_face))
+        pil_face = Image.fromarray(np_face, 'RGB')
+        # adapter la taille à notre CNN
         pil_face = pil_face.resize((49, 49), resample=Image.BILINEAR)
+        # remettre l'image et rgb
+        b, g, r = pil_face.split()
+        pil_face = Image.merge("RGB", (r, g, b))
 
-        pil_face.save('face_resized.png')
+        # pil_face.save('face_resized.png', format='PNG')
+        
+        # remettre tout en np.array
+        np_face = np.asarray(pil_face, dtype=np.uint8)
+        print(np_face.shape)
+
+        #call le predict
 
     cv2.imshow('img',img)
     # kill with ctr+c
