cb7bc81a6d882ed1cd3e52100c4c1feead3362cc
ArthurTlprt <arthur.talpaert@ien-lille.fr>
2017-11-17 15:04:11 +0100

    mirror fonctionne mais les smileys s'accumulent

--- demonstrator/mirror/index.js
+++ demonstrator/mirror/index.js
@@ -1,9 +1,13 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname +'/emoji/Anger.png');
 });
 
 io.on('connection', function(socket) {
